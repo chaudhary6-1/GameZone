2e9d01f0367b5f00b20f8cce93683c8ae6f7e3b6
Niklas <albrenik@b-tu.de>
2021-01-10 00:37:18 +0100

    JS change for new look

--- docs/gameData.js
+++ docs/gameData.js
@@ -112,9 +112,14 @@ function getGame(name) {
 			games.forEach(gameXmlNode => {
 				if (gameXmlNode.children[0].innerHTML.includes(name)) {
 					xmlContent2 += "<li>"
-								+ "<h2>" + gameXmlNode.children[0].innerHTML + "(" + gameXmlNode.children[4].innerHTML +  ")</h2>"
-								+ "<iframe width='560' height='315' src='" + gameXmlNode.children[3].innerHTML + "' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
-								+ "<div>" + gameXmlNode.children[1].innerHTML + "</div>"
+								+ "<h2>" + gameXmlNode.children[0].innerHTML + "<div class='category'>" + gameXmlNode.children[8].innerHTML + "</div></h2>"
+								+ "<div class='gamePic'><img src=" + gameXmlNode.children[2].innerHTML + " alt= Game Picture width='1366' height='768'>"
+								+ "<div class='publisher'>" + gameXmlNode.children[6].innerHTML + "</div>"
+								+ "<div class='release'" + gameXmlNode.children[4].innerHTML + "</div>"
+								+ "<div class='rating'" + gameXmlNode.children[7].innerHTML + "</div>"
+								+ "<div class='description'" + gameXmlNode.children[1].innerHTML + "</div>"
+								+ "<iframe width='1366' height='768' src='" + gameXmlNode.children[3].innerHTML + "' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+								+ "<div class='review'>" + gameXmlNode.children[5].innerHTML + "</div>"
 						+ "</li>";
 				}
 		
